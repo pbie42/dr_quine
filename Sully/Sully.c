8e15a7d47ca313299a0ca00527a39278e75d064a
Paul BIE <pbie@e1r7p4.42.fr>
2018-01-19 18:28:43 +0100

    [Add] - Working Sully

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -4,7 +4,7 @@ int main()
 {
 char *str[80];
 char *num[80];
-static int i = 1;
+static int i = 5;
 while (i > 0)
 {
 sprintf(num, "%d", --i);
@@ -14,8 +14,9 @@ strcat(str, ".c");
 FILE *a = fopen(str, "ab+");
 FILE *fp;
 fp = fopen(str, "w");
-char *s="#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%cchar *str[80];%cchar *num[80];%cstatic int i = 1;%cwhile (i > 0)%c{%csprintf(num, %c%%d%c, --i);%cstrcpy(str, %cSully_%c);%cstrcat(str, num);%cstrcat(str, %c.c%c);%cFILE *a = fopen(str, %cab+%c);%cFILE *fp;%cfp = fopen(str, %cw%c);%cfunction();%cchar *s=%c%s%c;%cfprintf(fp,s,10,10,10,10,10,34,34,34,34,10,10,34,34,34,34,10,10,10,34,s,34,10,10,10,10);%cfclose(fp);%c}%c";
-fprintf(fp,s,10,10,10,10,10,10,10,10,10,34,34,10,34,34,10,10,34,34,10,34,34,10,10,34,34,10,10,34,s,34,10,10,10,10);
+char *s="#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%cchar *str[80];%cchar *num[80];%cstatic int i = %d;%cwhile (i > 0)%c{%csprintf(num, %c%%d%c, --i);%cstrcpy(str, %cSully_%c);%cstrcat(str, num);%cstrcat(str, %c.c%c);%cFILE *a = fopen(str, %cab+%c);%cFILE *fp;%cfp = fopen(str, %cw%c);%cchar *s=%c%s%c;%cfprintf(fp,s,10,10,10,10,10,10,++i,10,10,10,34,34,10,34,34,10,10,34,34,10,34,34,10,10,34,34,10,34,s,34,10,10,10,10,10,10);%cfclose(fp);%c--i%c}%c}%c";
+fprintf(fp,s,10,10,10,10,10,10,++i,10,10,10,34,34,10,34,34,10,10,34,34,10,34,34,10,10,34,34,10,34,s,34,10,10,10,10,10,10);
 fclose(fp);
+--i;
 }
 }
