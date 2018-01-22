5b621882c7c0c6239395c1fbf46c56a1a22de22d
Paul Bie <synergized@iMac-de-Synergized.local>
2018-01-22 12:10:59 +0100

    [Refactor] - Useless line deleted in Sully.js

--- js/Sully.js
+++ js/Sully.js
@@ -1,8 +1,7 @@
 var { exec } = require("child_process");
 var path = require("path");
 var fs = require('fs');
-var sully = "Sully";
 var i = 5;
 if (path.basename(module.filename).split("_")[1]) i--;
-(function f() { /*suh bruh?*/ fs.writeFile(sully + '_' + i + '.js', 'var { exec } = require("child_process");\nvar path = require("path");\nvar fs = require(\'fs\');\nvar sully = "Sully";\nvar i = ' + (i) + ';\nif (path.basename(module.filename).split("_")[1]) i--;\n(' + f.toString() + ')()\nif (i > 0) exec(`node Sully_${i}.js`);', function(err) { if (err) throw err;} ); })()
+(function f() { /*suh bruh?*/ fs.writeFile('Sully_' + i + '.js', 'var { exec } = require("child_process");\nvar path = require("path");\nvar fs = require(\'fs\');\nvar i = ' + (i) + ';\nif (path.basename(module.filename).split("_")[1]) i--;\n(' + f.toString() + ')()\nif (i > 0) exec(`node Sully_${i}.js`);', function(err) { if (err) throw err;} ); })()
 if (i > 0) exec(`node Sully_${i}.js`);
