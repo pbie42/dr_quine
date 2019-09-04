03aab9a5bd65fd29e38decd324dad0643c22cf8b
Paul BIE <pbie@e1r7p6.42.fr>
2019-09-04 14:50:15 +0200

    [Fix] - Problem in Sully.js where Sully_-1.js would be created if Sully_0.j was launched

--- js/Sully.js
+++ js/Sully.js
@@ -2,6 +2,8 @@ var { exec } = require("child_process");
 var path = require("path");
 var fs = require('fs');
 var i = 5;
-if (path.basename(module.filename).split("_")[1]) i--;
-(function f() { /*suh bruh?*/ fs.writeFile('Sully_' + i + '.js', 'var { exec } = require("child_process");\nvar path = require("path");\nvar fs = require(\'fs\');\nvar i = ' + (i) + ';\nif (path.basename(module.filename).split("_")[1]) i--;\n(' + f.toString() + ')()\nif (i > 0) exec(`node Sully_${i}.js`);', function(err) { if (err) throw err;} ); })()
+var fPath = path.basename(module.filename).split("_")[1];
+if (i <= 0) return;
+if (fPath && fPath[0] > 0) i--;
+(function f() { /*suh bruh?*/ fs.writeFile('Sully_' + i + '.js', 'var { exec } = require("child_process");\nvar path = require("path");\nvar fs = require(\'fs\');\nvar i = ' + (i) + ';\nvar fPath = path.basename(module.filename).split("_")[1];\nif (i <= 0) return;\nif (fPath && fPath[0] > 0) i--;\n(' + f.toString() + ')()\nif (i > 0) exec(`node Sully_${i}.js`);\n', function(err) { if (err) throw err;} ); })()
 if (i > 0) exec(`node Sully_${i}.js`);
