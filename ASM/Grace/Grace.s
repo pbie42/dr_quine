800477ba3a884bc41939609ed383a0112a9c4029
Paul BIE <pbie@e1r8p17.42.fr>
2019-10-31 14:35:08 +0100

    [Fix] - Wrong definition in my grace.s

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -4,7 +4,7 @@
 ;suhhh dude
 section .data
 yo:
-	.msg db "%4$cdefine openFlags 0x0202%1$c%4$cdefine chmodPerm 0777q%1$c%4$c%macro doitfool 0%1$c;suhhh dude%1$csection .data%1$chello:%1$c	.msg db %2$c%3$s%2$c, 0%1$c	.len equ $ - yo.msg%1$c	filename db %2$c./Grace_kid.s%2$c, 0%1$c%1$csection .text%1$c	global start%1$c	global _main%1$c	extern _dprintf%1$c%1$cstart:%1$c	call _main%1$c	ret%1$c%1$c_main:%1$c	enter 16, 0%1$c%1$c	mov rdi, filename%1$c	mov rsi, openFlags%1$c	mov rax, 0x2000005%1$c	syscall%1$c	mov r12, rax%1$c%1$c	mov rax, 0x200000f%1$c	mov rsi, chmodPerm%1$c	lea rdi, [rel filename]%1$c	mov rdx, 10%1$c	syscall%1$c%1$c	mov rdi, r12%1$c	lea rsi, [rel yo.msg]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel yo.msg]%1$c	mov r9, 37%1$c	call _dprintf%1$c%1$c	leave%1$c	ret%1$c%4$cendmacro%1$c%1$cdoitfool%1$c", 0
+	.msg db "%4$cdefine openFlags 0x0202%1$c%4$cdefine chmodPerm 0777q%1$c%4$c%macro doitfool 0%1$c;suhhh dude%1$csection .data%1$cyo:%1$c	.msg db %2$c%3$s%2$c, 0%1$c	.len equ $ - yo.msg%1$c	filename db %2$c./Grace_kid.s%2$c, 0%1$c%1$csection .text%1$c	global start%1$c	global _main%1$c	extern _dprintf%1$c%1$cstart:%1$c	call _main%1$c	ret%1$c%1$c_main:%1$c	enter 16, 0%1$c%1$c	mov rdi, filename%1$c	mov rsi, openFlags%1$c	mov rax, 0x2000005%1$c	syscall%1$c	mov r12, rax%1$c%1$c	mov rax, 0x200000f%1$c	mov rsi, chmodPerm%1$c	lea rdi, [rel filename]%1$c	mov rdx, 10%1$c	syscall%1$c%1$c	mov rdi, r12%1$c	lea rsi, [rel yo.msg]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel yo.msg]%1$c	mov r9, 37%1$c	call _dprintf%1$c%1$c	leave%1$c	ret%1$c%4$cendmacro%1$c%1$cdoitfool%1$c", 0
 	.len equ $ - yo.msg
 	filename db "./Grace_kid.s", 0
 
